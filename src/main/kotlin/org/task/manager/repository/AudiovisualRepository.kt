242916759560182fff48ccf2a54a77286ff04ed4
david <david>
2020-05-24 13:55:47 +0200

    Add to send notifications to notify the user about on start and end dates

--- src/main/kotlin/org/task/manager/repository/AudiovisualRepository.kt
+++ src/main/kotlin/org/task/manager/repository/AudiovisualRepository.kt
@@ -1,5 +1,6 @@
 package org.task.manager.repository
 
+import java.time.Instant
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
@@ -16,4 +17,8 @@ interface AudiovisualRepository : JpaRepository<Audiovisual, Long> {
 
     @Query("select audiovisual from Audiovisual audiovisual where audiovisual.user.login = ?#{principal.username}")
     fun findByUserIsCurrentUser(pageable: Pageable): Page<Audiovisual>
+
+    fun findAllByStartDateBetweenAndCheck(fromDate: Instant, toDate: Instant, check: Int): List<Audiovisual>
+
+    fun findAllByDeadlineBetweenAndCheck(fromDate: Instant, toDate: Instant, check: Int): List<Audiovisual>
 }
