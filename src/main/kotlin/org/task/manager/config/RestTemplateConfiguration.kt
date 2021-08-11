7a410a3680fa42b8c1877de4c3fb072614a07d32
Steampunk <dasies11@gmail.com>
2021-08-11 20:34:41 +0200

    Add resttemplate as client to invoke external providers

---
+++ src/main/kotlin/org/task/manager/config/RestTemplateConfiguration.kt
@@ -0,0 +1,15 @@
+package org.task.manager.config
+
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class RestTemplateConfiguration {
+
+    @Bean
+    fun restTemplate(): RestTemplate {
+        return RestTemplateBuilder().build()
+    }
+}
