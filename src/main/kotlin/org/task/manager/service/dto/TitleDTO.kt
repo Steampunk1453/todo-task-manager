4de47c652a2f91c7c6e23b1c588b1f96c9a60ed9
Steampunk <dasies11@gmail.com>
2021-08-28 13:03:00 +0200

    Update validations to get platform

--- src/main/kotlin/org/task/manager/service/dto/TitleDTO.kt
+++ src/main/kotlin/org/task/manager/service/dto/TitleDTO.kt
@@ -4,6 +4,9 @@ import org.task.manager.domain.TitleInfo
 import java.net.URI
 import java.net.URISyntaxException
 
+private const val PATTERN_ALL_AFTER_LAST_DOT = ".[^.]*$"
+private const val APPLE_TV =  "Apple TV+"
+
 data class TitleDTO(
     val title: String,
     val type: Type,
@@ -16,15 +19,26 @@ fun TitleInfo.toDto(): TitleDTO = TitleDTO(
     title = title,
     type = Type.fromString(type.toString()),
     genres = genres?.split(","),
-    platform =  if (website?.isNotEmpty() == true) getDomainName(website!!) else  "",
+    platform = if (website?.isNotEmpty() == true) getPlatformFromUrl(website!!) else null,
     website = website
 )
 
 @Throws(URISyntaxException::class)
-private fun getDomainName(url: String): String {
+private fun getPlatformFromUrl(url: String): String {
     val uri = URI(url)
-    val domain = uri.host
-    return if (domain.startsWith("www.")) domain.substring(4, domain.lastIndexOf(".")).capitalize() else domain.capitalize()
+    val host = uri.host
+    return extractPlatform(host)
+}
+
+private fun extractPlatform(host: String): String {
+    var platform = if (host.startsWith("www.")) host.substring(4, host.lastIndexOf(".")).capitalize() else host.capitalize()
+    platform = if (platform.contains(".")) platform.replace(PATTERN_ALL_AFTER_LAST_DOT.toRegex(), "") else platform
+    platform = checkAppleTv(platform)
+    return platform
+}
+
+private fun checkAppleTv(platform: String): String {
+    return if (platform.toLowerCase().contains("apple")) APPLE_TV else platform
 }
 
 enum class Type(val value: String) {
